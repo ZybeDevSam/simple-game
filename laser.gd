2b539237436e33244ecc3b96d92f580716726c1e
Sam Belin <belinsam406@gmail.com>
2026-02-02 13:58:21 -0700

    feat: add laser

--- laser.gd
+++ laser.gd
@@ -2,10 +2,15 @@ extends Area2D
 
 @export var speed = 500
 
-# func _ready() -> void:
+func _ready() -> void:
+	if speed == null:
+		speed = 500
+	var tween = create_tween()
+	tween.tween_property($Sprite2D, 'scale', Vector2(1,1), 0.2).from(Vector2(0,0))
 # 	await get_tree().create_timer(5.0).timeout
 # 	queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y -= speed * delta
+	var sp = speed if speed != null else 500
+	position.y -= sp * delta
--- level.gd
+++ level.gd
@@ -3,6 +3,23 @@ extends Node2D
 var meteor_scene: PackedScene = load("res://meteor.tscn")
 var laser_scene: PackedScene = load("res://laser.tscn")
 
+func _ready() -> void:
+	#stars
+	var size := get_viewport().get_visible_rect().size
+	var rng := RandomNumberGenerator.new()
+	for star in $Stars.get_children():
+		#position
+		var random_x = rng.randi_range(0, int(size.x))
+		var random_y = rng.randi_range(0, int(size.y))
+		star.position = Vector2(random_x, random_y)
+
+		#scale
+		var random_scale = rng.randf_range(1,2)
+		star.scale = Vector2(random_scale, random_scale)
+
+		#animation speed
+		star.speed_scale = rng.randf_range(0.6,1.4)
+
 func _on_meteor_timer_timeout() -> void:
 	var meteor = meteor_scene.instantiate()
 
